993f8596e40c3ac5dcb91861aea6a0fe9db90ee0
Krazel <krazelpausalinter@email.com>
2026-08-10 20:42:58 +0200

    fix: declare macOS baseline for core tests

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "ZodiacDailyCore",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v16),
+        .macOS(.v12)
     ],
     products: [
         .library(
